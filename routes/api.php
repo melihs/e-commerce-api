ef822928d289cf9be0bcb0c3567e8c7282727d6b
melihs <melihsahin24@gmail.com>
2019-06-13 22:02:19 +0300

    product and review defined model and routes

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,16 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+	return $request->user();
+
+});
+
+
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix'=>'products'],function (){
+
+	Route::apiResource('/{product}/reviews','ReviewController');
+
 });
