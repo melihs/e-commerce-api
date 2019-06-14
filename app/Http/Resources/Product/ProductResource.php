5374814ea536abea0e329502f4ad38c66875bb7f
melihs <melihsahin24@gmail.com>
2019-06-15 00:41:28 +0300

    Modified Product Collection resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductResource extends JsonResource
+class ProductResource extends Resource
 {
     /**
      * Transform the resource into an array.
@@ -15,6 +15,7 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
+        	'id' => $this->id,
         	'name' => $this->name,
         	'description' => $this->detail,
         	'price' => $this->price,
