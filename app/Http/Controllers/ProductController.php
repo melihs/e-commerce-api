feee22ad2871226de304bfb55e35510060fec5bc
melihs <melihsahin24@gmail.com>
2019-06-17 12:39:33 +0300

    Product destroy completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -83,6 +83,9 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+    	$product->delete();
+
+	    return response(null,204);
+
     }
 }
